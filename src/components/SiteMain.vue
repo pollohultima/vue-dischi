2da525db62556e3a15f116d69185e755571abfdf
pollohultima <matteo.polonio@gmail.com>
2021-11-15 12:32:27 +0100

    Completing cards css

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,8 +1,39 @@
-<template></template>
+<template>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-2" v-for="album in albums" :key="album.title">
+          <div class="card">
+            <img :src="album.poster" alt="" />
+            <h6>{{ album.title }}</h6>
+            <p>{{ album.author }}</p>
+            <p>{{ album.year }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((r) => {
+        this.albums = r.data.response;
+      });
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+.card {
+}
 </style>
