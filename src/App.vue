5f0019a0e5136e1ab5716f4b311bf50cc5424c94
pollohultima <matteo.polonio@gmail.com>
2021-11-15 11:56:14 +0100

    Header completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SiteHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SiteHeader from "./components/SiteHeader.vue";
+//import SiteMain from "./components/SiteMain.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SiteHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="d-flex align-items-center">
+    <div class="logo px-4">
+      <img src="../assets/logo.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+header {
+  height: 70px;
+  background-color: #2e3a46;
+  .logo {
+    height: 45px;
+    img {
+      height: 100%;
+    }
+  }
+}
+</style>
